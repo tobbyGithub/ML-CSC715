9b9d34459c54a4e091852e8e6c9a789652c19f04
tobbyGithub <amodutobby@gmail.com>
2017-11-21 18:10:05 +0100

    numpy

---
+++ linreg.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+my_array=[[1,4],[54,231],[12,'bitch']]
+print(my_array)
+num_array= np.array(my_array)
+print(num_array)
+print('20 elements are')
+NUONE=np.random.randint(0,30,20)
+print(NUONE)
+print(NUONE.max())
