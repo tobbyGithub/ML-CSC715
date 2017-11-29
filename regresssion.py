3e8feb5395c5ac7feb5e246692e70d52a9e7eee1
tobby <amodutobby@gmail.com>
2017-11-29 06:38:05 +0100

    dataset intro

--- regresssion.py
+++ regresssion.py
@@ -1,57 +1,74 @@
+'''
+CODE TO CALCULATE SIMPLE LINEAR REGRESSION ON DATASET WITH JUST 2 VARIABLES
+SAY X. Y
 
-# Estimate Mean and Variance
- 
-# Calculate the mean value of a list of numbers
+x | y
+-----
+1 | 2
+2 | 3
+4 | 3
+3 | 2
+5 | 5
+------
+'''
+
+
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+
+# Using Numpy..generate our dataset using a series of random values.
+import Numpy as np
+
+
+# a mfunction that Calculates the mean value of a list of numbers by calculating the sum and dividing it by the total count 'n' of numbers
 def mean(values):
 	return sum(values) / float(len(values))
  
-# Calculate the variance of a list of numbers
-def variance(values, mean):
-	return sum([(x-mean)**2 for x in values])
+# # Calculate the variance of a list of numbers
+# def variance(values, mean):
+# 	return sum([(x-mean)**2 for x in values])
  
-# calculate mean and variance
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-x = [row[0] for row in dataset]
-y = [row[1] for row in dataset]
-mean_x, mean_y = mean(x), mean(y)
-var_x, var_y = variance(x, mean_x), variance(y, mean_y)
-print('x stats: mean=%.3f variance=%.3f' % (mean_x, var_x))
-print('y stats: mean=%.3f variance=%.3f' % (mean_y, var_y))
+# # calculate mean and variance
+# x = [row[0] for row in dataset]
+# y = [row[1] for row in dataset]
+# mean_x, mean_y = mean(x), mean(y)
+# var_x, var_y = variance(x, mean_x), variance(y, mean_y)
+# print('x stats: mean=%.3f variance=%.3f' % (mean_x, var_x))
+# print('y stats: mean=%.3f variance=%.3f' % (mean_y, var_y))
 
 
-# Calculate covariance between x and y
-def covariance(x, mean_x, y, mean_y):
-	covar = 0.0
-	for i in range(len(x)):
-		covar += (x[i] - mean_x) * (y[i] - mean_y)
-	return covar
-
-
-
-
-# calculate covariance
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-x = [row[0] for row in dataset]
-y = [row[1] for row in dataset]
-mean_x, mean_y = mean(x), mean(y)
-covar = covariance(x, mean_x, y, mean_y)
-print('Covariance: %.3f' % (covar))
+# # Calculate covariance between x and y
+# def covariance(x, mean_x, y, mean_y):
+# 	covar = 0.0
+# 	for i in range(len(x)):
+# 		covar += (x[i] - mean_x) * (y[i] - mean_y)
+# 	return covar
 
 
 
 
-
-# Calculate coefficients
-def coefficients(dataset):
-	x = [row[0] for row in dataset]
-	y = [row[1] for row in dataset]
-	x_mean, y_mean = mean(x), mean(y)
-	b1 = covarianxce(x, x_mean, y, y_mean) / variance(x, x_mean)
-	b0 = y_mean - b1 * x_mean
-	return [b0, b1]
+# # calculate covariance
+# dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+# x = [row[0] for row in dataset]
+# y = [row[1] for row in dataset]
+# mean_x, mean_y = mean(x), mean(y)
+# covar = covariance(x, mean_x, y, mean_y)
+# print('Covariance: %.3f' % (covar))
 
 
-# calculate coefficients
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-b0, b1 = coefficients(dataset)
-print('Coefficients: B0=%.3f, B1=%.3f' % (b0, b1))
+
+
+
+# # Calculate coefficients
+# def coefficients(dataset):
+# 	x = [row[0] for row in dataset]
+# 	y = [row[1] for row in dataset]
+# 	x_mean, y_mean = mean(x), mean(y)
+# 	b1 = covarianxce(x, x_mean, y, y_mean) / variance(x, x_mean)
+# 	b0 = y_mean - b1 * x_mean
+# 	return [b0, b1]
+
+
+# # calculate coefficients
+# dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+# b0, b1 = coefficients(dataset)
+# print('Coefficients: B0=%.3f, B1=%.3f' % (b0, b1))
